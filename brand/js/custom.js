90a00a1a98942f4b857fa43aad954075e694fb2f
Nion Roy <nion.roy22@gmail.com>
2023-02-07 13:14:30 +0600

    new added

--- brand/js/custom.js
+++ brand/js/custom.js
@@ -1,10 +1,10 @@
 $(function () {
-   $("[data-btn]").click(button);
+  $("[data-btn]").click(button);
 });
 
 function button() {
-   $(this).parent().find("[data-spinner]").addClass("active");
-   setTimeout(() => {
-      $(this).parent().find("[data-spinner]").removeClass("active");
-   }, 5000);
+  $(this).parent().find("[data-spinner]").addClass("active");
+  setTimeout(() => {
+    $(this).parent().find("[data-spinner]").removeClass("active");
+  }, 5000);
 }
